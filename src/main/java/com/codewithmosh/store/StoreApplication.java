54d0bcdd9ffd8994fe87e6bde97a32a1f8f36df9
Roller Lo <jasonbh02@gmail.com>
2025-08-06 15:37:04 +0800

    refactor(OrderService): setter injection

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -7,7 +7,8 @@ public class StoreApplication {
 
     public static void main(String[] args) {
 //        SpringApplication.run(StoreApplication.class, args);
-        var orderService = new OrderService(new PayPalPaymentService());
+        var orderService = new OrderService();
+        orderService.setPaymentService(new PayPalPaymentService());
         orderService.placeOrder();
     }
 
