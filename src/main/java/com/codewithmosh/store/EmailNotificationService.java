9c24ac99c445a54144fad57f9db16ce9c9fb4ed1
Roller Lo <jasonbh02@gmail.com>
2025-08-06 22:54:10 +0800

    feat(NotificationManager): send email or sms by service notation

---
+++ src/main/java/com/codewithmosh/store/EmailNotificationService.java
@@ -0,0 +1,13 @@
+package com.codewithmosh.store;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service("email")
+@Primary
+public class EmailNotificationService implements NotificationService {
+    @Override
+    public void send(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
---
+++ src/main/java/com/codewithmosh/store/NotificationService.java
@@ -0,0 +1,5 @@
+package com.codewithmosh.store;
+
+public interface NotificationService {
+    void send(String message);
+}
