2758d4e495f18fc8cc6518b98be4237bbdfbeb7f
Roller Lo <jasonbh02@gmail.com>
2025-08-06 15:29:20 +0800

    refactor(PaymentService): constructor injection

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,13 +1,14 @@
 package com.codewithmosh.store;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StoreApplication.class, args);
+//        SpringApplication.run(StoreApplication.class, args);
+        var orderService = new OrderService(new PayPalPaymentService());
+        orderService.placeOrder();
     }
 
 }
