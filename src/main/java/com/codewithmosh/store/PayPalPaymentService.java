9f4c76ff4979ce94e5ea764e8341dd1014a1eddb
Roller Lo <jasonbh02@gmail.com>
2025-08-06 16:36:19 +0800

    refactor(OrderService): configuring beans using annotations

--- src/main/java/com/codewithmosh/store/PayPalPaymentService.java
+++ src/main/java/com/codewithmosh/store/PayPalPaymentService.java
@@ -1,5 +1,8 @@
 package com.codewithmosh.store;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PayPalPaymentService implements PaymentService {
     @Override
     public void processPayment(double amount) {
--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,14 +1,15 @@
 package com.codewithmosh.store;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(StoreApplication.class, args);
-        var orderService = new OrderService();
-        orderService.setPaymentService(new PayPalPaymentService());
+        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        var orderService = context.getBean(OrderService.class);
         orderService.placeOrder();
     }
 
