41fbe1ce8d7980b632af3ff1422d6c323a0643ac
Roller Lo <jasonbh02@gmail.com>
2025-08-07 14:58:58 +0800

    feat(OrderService): call method after bean created
    use `@PostConstruct` to call method

--- src/main/java/com/codewithmosh/store/OrderService.java
+++ src/main/java/com/codewithmosh/store/OrderService.java
@@ -1,5 +1,7 @@
 package com.codewithmosh.store;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 //@Service
@@ -9,6 +11,11 @@ public class OrderService {
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
         System.out.println("Order service created");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Order service PostConstruct");
     }
 
     public void placeOrder() {
