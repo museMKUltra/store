359e6a3c35d4312f70719ae46929e1b88d34e594
Roller Lo <jasonbh02@gmail.com>
2025-08-12 18:32:20 +0800

    feat(User): initializing user with constructor

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,13 +1,16 @@
 package com.codewithmosh.store;
 
-import org.springframework.boot.SpringApplication;
+import com.codewithmosh.store.entities.User;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
+        var user = new User(1L, "name", "email", "password");
+        user.setName("John");
+        user.setEmail("john@example.com");
+        user.setPassword("password");
     }
 
 }
--- src/main/java/com/codewithmosh/store/entities/User.java
+++ src/main/java/com/codewithmosh/store/entities/User.java
@@ -1,11 +1,15 @@
 package com.codewithmosh.store.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
