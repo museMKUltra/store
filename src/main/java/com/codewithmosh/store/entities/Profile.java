ace41f9c322550af85fabc07d9d6bbaa3a00469d
Roller Lo <jasonbh02@gmail.com>
2025-08-13 14:23:52 +0800

    feat: add relationship between User & Profile

--- src/main/java/com/codewithmosh/store/entities/Profile.java
+++ src/main/java/com/codewithmosh/store/entities/Profile.java
@@ -27,4 +27,9 @@ public class Profile {
 
     @Column(name = "loyalty_points")
     private Integer loyaltyPoints;
+
+    @OneToOne
+    @JoinColumn(name = "id")
+    @MapsId
+    private User user;
 }
