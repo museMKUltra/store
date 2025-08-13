342b8cd26f1068a91cf0a1b8f41747193e2b4646
Roller Lo <jasonbh02@gmail.com>
2025-08-13 15:18:30 +0800

    feat: create Category & Product entities

---
+++ src/main/java/com/codewithmosh/store/entities/Category.java
@@ -0,0 +1,19 @@
+package com.codewithmosh.store.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Byte id;
+
+    @Column(name = "name")
+    private String name;
+}
