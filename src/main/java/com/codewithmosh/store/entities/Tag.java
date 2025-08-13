ceb49a8155ce4d4f3e1719769fd42bf2ddc31406
Roller Lo <jasonbh02@gmail.com>
2025-08-13 13:17:44 +0800

    feat(Tag): show attributes of Tag

--- src/main/java/com/codewithmosh/store/entities/Tag.java
+++ src/main/java/com/codewithmosh/store/entities/Tag.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -12,6 +13,7 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 @Table(name = "tags")
 public class Tag {
     @Id
