8638b371f0108c638d4b1ab2deb4d870f2a0500c
Roller Lo <jasonbh02@gmail.com>
2025-08-14 14:44:02 +0800

    feat: create a user by repository

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -2,22 +2,24 @@ package com.codewithmosh.store;
 
 import com.codewithmosh.store.entities.Product;
 import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.repositories.UserRepository;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        var repository = context.getBean(UserRepository.class);
         var user = User.builder()
-                .id(1L)
                 .name("John")
                 .email("john@example.com")
                 .password("password")
                 .build();
 
-        user.getWishlist().add(new Product());
-
-        System.out.println(user);
+        repository.save(user);
     }
 
 }
