177262977016ab7ff297182afecef87ccf709640
Roller Lo <jasonbh02@gmail.com>
2025-08-15 15:56:48 +0800

    feat: create product with existing category

--- src/main/java/com/codewithmosh/store/services/ProductService.java
+++ src/main/java/com/codewithmosh/store/services/ProductService.java
@@ -2,6 +2,7 @@ package com.codewithmosh.store.services;
 
 import com.codewithmosh.store.entities.Category;
 import com.codewithmosh.store.entities.Product;
+import com.codewithmosh.store.repositories.CategoryRepository;
 import com.codewithmosh.store.repositories.ProductRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
     public void createProductWithNewCategory() {
         var product = Product.builder()
@@ -24,6 +26,21 @@ public class ProductService {
         var category = Category.builder()
                 .name("New Category")
                 .build();
+
+        product.addCategory(category);
+
+        productRepository.save(product);
+    }
+
+    @Transactional
+    public void createProductWithExistingCategory() {
+        var product = Product.builder()
+                .name("New Product")
+                .description("New Product")
+                .price(BigDecimal.valueOf(123))
+                .build();
+
+        var category = categoryRepository.findById(1L).orElseThrow();
 
         product.addCategory(category);
 
