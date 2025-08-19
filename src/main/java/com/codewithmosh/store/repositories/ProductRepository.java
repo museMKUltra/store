05e7c8b584b08b40f5dc327ff3389107bd16684d
Roller Lo <jasonbh02@gmail.com>
2025-08-19 15:52:38 +0800

    feat(ProductREpository): derived query methods

--- src/main/java/com/codewithmosh/store/repositories/ProductRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/ProductRepository.java
@@ -3,5 +3,37 @@ package com.codewithmosh.store.repositories;
 import com.codewithmosh.store.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product, Long> {
+    // String
+    List<Product> findByName(String name); // select * from product where name = ?
+    List<Product> findByNameLike(String name); // select * from product where name like ?
+    List<Product> findByNameNotLike(String name);
+    List<Product> findByNameContaining(String name);
+    List<Product> findByNameStartingWith(String name);
+    List<Product> findByNameEndingWith(String name);
+    List<Product> findByNameEndingWithIgnoreCase(String name);
+
+    // Numbers
+    List<Product> findByPrice(BigDecimal price); // select * from product where price = ?
+    List<Product> findByPriceGreaterThan(BigDecimal price);
+    List<Product> findByPriceGreaterThanEqual(BigDecimal price);
+    List<Product> findByPriceLessThanEqual(BigDecimal price);
+    List<Product> findByPriceBetween(BigDecimal min, BigDecimal max);
+
+    // Null
+    List<Product> findByDescriptionNull();
+    List<Product> findByDescriptionNotNull();
+
+    // Multiple conditions
+    List<Product> findByDescriptionNullAndNameNull();
+
+    // Sort (OrderBy)
+    List<Product> findByNameOrderByPrice(String name);
+
+    // Limit (Top/First)
+    List<Product> findTop5ByNameOrderByPrice(String name);
+    List<Product> findFirst5ByNameLikeOrderByPrice(String name);
 }
