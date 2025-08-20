205319d571239c10a8f777d0fd590bb1d703f3d4
Roller Lo <jasonbh02@gmail.com>
2025-08-20 14:53:12 +0800

    feat(UserRepository): fetch user with columns of addresses

--- src/main/java/com/codewithmosh/store/repositories/UserRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/UserRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    @EntityGraph(attributePaths = "tags")
+    @EntityGraph(attributePaths = {"tags", "addresses"})
     Optional<User> findByEmail(String email);
 }
