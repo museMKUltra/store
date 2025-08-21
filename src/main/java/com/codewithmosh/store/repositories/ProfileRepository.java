57001c24aeebf35452201e4199dec468c2d51aa8
Roller Lo <jasonbh02@gmail.com>
2025-08-21 13:08:18 +0800

    refactor(exercise): step 4. extract JPQL query

--- src/main/java/com/codewithmosh/store/repositories/ProfileRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/ProfileRepository.java
@@ -2,11 +2,14 @@ package com.codewithmosh.store.repositories;
 
 import com.codewithmosh.store.entities.Profile;
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProfileRepository extends CrudRepository<Profile, Long> {
+    @Query("select p from Profile p where p.loyaltyPoints > :points order by p.user.email")
     @EntityGraph(attributePaths = "user")
-    List<Profile> findByLoyaltyPointsGreaterThanOrderByUserEmail(Integer points);
+    List<Profile> findLoyalProfiles(@Param("points") Integer points);
 }
