9c3b51c1d0cc43a5574deb0dd8a97f78c3f488b3
Roller Lo <jasonbh02@gmail.com>
2025-08-21 13:04:49 +0800

    fix(exercise): step 2. solve n + 1 problem

--- src/main/java/com/codewithmosh/store/repositories/ProfileRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/ProfileRepository.java
@@ -1,10 +1,12 @@
 package com.codewithmosh.store.repositories;
 
 import com.codewithmosh.store.entities.Profile;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ProfileRepository extends CrudRepository<Profile, Long> {
+    @EntityGraph(attributePaths = "user")
     List<Profile> findByLoyaltyPointsGreaterThan(Integer points);
 }
